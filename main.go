eb9a4a6083ddc18ebed5f3ba9eac4b0a7dd23a8c
rzfd <rizkifadillah783@gmail.com>
2024-09-25 01:01:44 +0700

    Feat: add main.go

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+
+	"github.com/rzfd/finance-test/internal/handlers/http/controller"
+	"github.com/rzfd/finance-test/internal/utill"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbConn := utill.ConnectDB()
+
+	e := echo.New()
+
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	e.POST("/register", controller.Register(dbConn, jwtSecret))
+	e.POST("/login", controller.Login(dbConn, jwtSecret))
+
+	protected := e.Group("")
+	protected.Use(utill.JWTMiddleware(jwtSecret))
+	protected.POST("/topup", controller.TopUp(dbConn))
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
